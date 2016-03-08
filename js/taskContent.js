7cff3a46c820b77fb23056ced869fe1c11cd89c3
liuliaopu <liuliaopu@bbktel.com>
2016-03-08 11:08:55 +0800

    修改大标题交互逻辑

--- js/taskContent.js
+++ js/taskContent.js
@@ -12,16 +12,19 @@ $(document).ready(function(){
 function translateToEmptyTask(){
   $("#tab_group").children("div.active").removeClass("active");
   $("#task_none_tab").addClass("active");
+  $("#save_all_btn").css("display", "none");
 }
 
 function translateToViewTask() {
   $("#new_or_save_task_btn").text("新建任务");
   $("#tab_group").children("div.active").removeClass("active");
   $("#task_view_tab").addClass("active");
+  $("#save_all_btn").css("display", "none");
 }
 
 function translateToEditTask() {
   $("#new_or_save_task_btn").text("保存任务");
+  $("#save_all_btn").css("display", "inline");
   $("#tab_group").children("div.active").removeClass("active");
   $("#task_edit_tab").addClass("active");
   initializeEditorIfNeeded();
--- js/taskTitle.js
+++ js/taskTitle.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#edit_or_save_title_btn").click(function(){
     var value = $("#edit_or_save_title_btn").text();
-    if (value == "编辑标题") {
+    if (value == "编辑大标题") {
       translateToEditTitle();
     } else {
       translateToSaveTitle();
@@ -10,10 +10,15 @@ $(document).ready(function(){
 });
 
 function translateToEditTitle(){
-  $("#edit_or_save_title_btn").text("保存标题");
-
+  $("#edit_or_save_title_btn").text("保存大标题");
+  $("#header_input_title").val($("#header_view_title").text());
+  $("#header_view_title").css("display", "none");
+  $("#header_input_title_container").css("display", "inline");
 }
 
 function translateToSaveTitle(){
-  $("#edit_or_save_title_btn").text("编辑标题");
+  $("#edit_or_save_title_btn").text("编辑大标题");
+  $("#header_input_title_container").css("display", "none");
+  $("#header_view_title").text($("#header_input_title").val());
+  $("#header_view_title").css("display", "inline");
 }
