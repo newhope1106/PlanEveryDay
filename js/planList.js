2cb6513252316d8001aa284681d774f0a425c33b
newhope1106 <408705420@qq.com>
2016-03-09 22:16:17 +0800

    添加动态添加计划安排的接口

---
+++ js/planList.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  initialPlanListTestData();
+});
+
+function initialPlanListTestData(){
+  var length = 10;
+  var planListView = $("#list_items_container");
+  planListView.empty();
+
+   for(var i=1; i<=length; i++) {
+     var planListItem = createPlanListItem("2016年第" + i + "周工作安排",
+      "创建日期：2016-01-" + (i<10?"0"+i:i));
+     planListView.append(planListItem);
+   }
+}
+
+function createPlanListItem(planTitle, planDescription){
+  return $('<div class="item"><i class="file icon"></i>'
+          +'<div class="content"><div class="header">'
+          + planTitle + '</div><div class="description">'
+          + planDescription + '</div></div></div>');
+}
