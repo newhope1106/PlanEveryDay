8046ffb8dc707c17bf17009fa0248d2a107504d4
newhope1106 <408705420@qq.com>
2016-03-16 11:07:48 +0800

    解决菜单无作用问题

--- js/menu.js
+++ js/menu.js
@@ -3,79 +3,54 @@ const Menu = remote.Menu;
 
 var template = [
   {
-    label: '新建',
-    click: function(item, focusedWindow) {
-    }
-  },
-  {
-    label: '编辑',
-    click: function(item, focusedWindow) {
+    label:"菜单",
+      submenu :[
+      {
+        label: '新建计划',
+        click: function(item, focusedWindow) {
+          showPlanPopup();
+        }
+      },
+      {
+        label: '统计',
+        click: function(item, focusedWindow) {
 
-    }
-  },
-  {
-    label: '查看',
-    click: function(item, focusedWindow) {
+        }
+      },
+      {
+        label: '帮助',
+        click: function(item, focusedWindow) {
 
-    }
-  },
-  {
-    label: '删除',
-    click: function(item, focusedWindow) {
-
-    }
-  },
-  {
-    label: '统计',
-    click: function(item, focusedWindow) {
-
-    }
-  },
-  {
-    label: '帮助',
-    click: function(item, focusedWindow) {
-
-    }
+        }
+      }
+    ]
   }
 ];
 
 if (process.platform == 'darwin') {
   var name = require('electron').remote.app.getName();
   template.unshift({
-    label: '新建',
-    click: function(item, focusedWindow) {
+    label:"菜单",
+      submenu :[
+      {
+        label: '新建计划',
+        click: function(item, focusedWindow) {
 
-    }
-  },
-  {
-    label: '编辑',
-    click: function(item, focusedWindow) {
+        }
+      },
+      {
+        label: '统计',
+        click: function(item, focusedWindow) {
 
-    }
-  },
-  {
-    label: '查看',
-    click: function(item, focusedWindow) {
+        }
+      },
+      {
+        label: '帮助',
+        click: function(item, focusedWindow) {
 
-    }
-  },
-  {
-    label: '删除',
-    click: function(item, focusedWindow) {
-
-    }
-  },
-  {
-    label: '统计',
-    click: function(item, focusedWindow) {
-
-    }
-  },
-  {
-    label: '帮助',
-    click: function(item, focusedWindow) {
-
-    }
+        }
+      }
+    ]
   });
 }
 
