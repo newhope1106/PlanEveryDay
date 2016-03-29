832e97ab53c7a7fa5e6d49291fe86038a84b1901
newhope1106 <408705420@qq.com>
2016-03-29 10:56:35 +0800

    去掉时间选择器的一些特殊节日

--- js/datePicker.js
+++ js/datePicker.js
@@ -13,44 +13,6 @@ $(document).ready(function(){
       format: 'yyyy-mm-dd',
       width: 280,
       height: 280,
-      data: [
-        {
-          date: '2015/12/24',
-          value: 'Christmas Eve'
-        },
-        {
-          date: '2015/12/25',
-          value: 'Merry Christmas'
-        },
-        {
-          date: '2016/01/01',
-          value: 'Happy New Year'
-        },
-        {
-          date: '2016/03/08',
-          value : 'Happy Women\'s Day'
-        },
-        {
-          date: '2016/04/01',
-          value : 'April Fools\' Day'
-        },
-        {
-          date: '2016/05/01',
-          value : 'Labour Day'
-        },
-        {
-          date: '2016/06/01',
-          value : 'Happy Children\'s Day'
-        },
-        {
-          date: '2016/09/10',
-          value : 'Happy Teacher\'s Day'
-        },
-        {
-          date: '2016/10/01',
-          value : 'National Day'
-        }
-      ],
       onSelected: function (view, date, data) {
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
